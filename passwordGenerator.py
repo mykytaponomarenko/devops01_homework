90ed16b8d8d73d173311ee8dbb4cd157bf0491b1
MykytaPonomarenko <mykyta.ponomarenko@gmail.com>
2023-06-28 23:37:21 +0300

    password generator is ready

--- passwordGenerator.py
+++ passwordGenerator.py
@@ -1,11 +1,36 @@
 import string
+import random
 
-uppercase = string.ascii_uppercase
-lowercase = string.ascii_lowercase
-numbers = string.digits
-special = string.punctuation
+def generatePassword(passwordLength):
+
+# Initiate all types of characters. Each value is string
+    if int(passwordLength) >= 4:
+        uppercase = string.ascii_uppercase
+        lowercase = string.ascii_lowercase
+        numbers = string.digits
+        special = string.punctuation
+        allSymbols = uppercase + lowercase + numbers + special # concatenate all symbols  
+        pas = random.choice(uppercase) + random.choice(lowercase) + random.choice(numbers) + random.choice(special)
+        symbolsGenerationCount = int(passwordLength) - 4
+        print(str(symbolsGenerationCount) + " characters to be generated in order to complete the password")
+        while symbolsGenerationCount != 0:
+            pas = pas + random.choice(allSymbols)
+            symbolsGenerationCount -= 1
+
+    else:
+        pas = "Requested password lenght is less than 4 characters"
+
+    return pas
+
+passwordLength = input("Type desired password length (at least 4 symbols) and press enter\n")
+
+password = generatePassword(passwordLength)    
+print(password + ", requested length: " + passwordLength + ", actual lenght: " + str(len(password)))
+
+
 
 #print(type(uppercase))
+
 
 # shows all english uppercase letters
 #for u in uppercase:
