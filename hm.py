3ecd914d9e45863623801b357346a8b604dd30d4
MykytaPonomarenko <mykyta.ponomarenko@gmail.com>
2023-06-28 18:13:25 +0300

    defined math functions

--- hm.py
+++ hm.py
@@ -1,27 +1,59 @@
+# Addition function
+def addition (a, b):
+    result = int(a) + int(b)
+    return str(result)
+
+# substraction function
+def substraction (a,b):
+    result = int(a) - int(b)
+    return str(result)
+
+# multiplication function
+def multiplication (a,b):
+    result = int(a) * int(b)
+    return str(result)
+
+# division function
+def division (a,b):
+    if b == "0":
+        result = "You can't divide by 0, don't be a fool :)"
+    else: 
+        result = int(a) / int(b)
+    return str(result)
+
+
 print("Welcome to PyCalculator!") #welcome message
 
+# Collect values from user  
 firstNumber = input("Type first integer and press enter\n")
 print("-------------------\nWell done, you typed '" + firstNumber + "'\n-------------------")
 
 secondNumber = input("Type second integer and press enter\n")
 print("-------------------\nWell done, you typed '" + secondNumber + "'\n-------------------")
 
+# User selects operation type
+
 operationType = input("In order to select operation type related number:\n1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n")
 
 if operationType == "1":
-    print("-------------------\nAddition it is! Result is " + str(int(firstNumber) + int(secondNumber)) + "\n-------------------")
+# Activate Addition flow
+    result = addition(firstNumber, secondNumber)
+    print("-------------------\nAddition it is! Result is '" + result + "'\n-------------------")
 
 elif operationType == "2":
-    print("-------------------\nSubstraction it is! Result is " + str(int(firstNumber) - int(secondNumber)) + "\n-------------------")
+# Activate Substraction flow
+    result = substraction(firstNumber, secondNumber)
+    print("-------------------\nSubstraction it is! Result is '" + result + "'\n-------------------")
 
 elif operationType == "3":
-    print("-------------------\nMultiplication it is! Result is " + str(int(firstNumber) * int(secondNumber)) + "\n-------------------")
+# Activate Multiplication flow
+    result = multiplication(firstNumber, secondNumber)
+    print("-------------------\nMultiplication it is! Result is '" + result + "'\n-------------------")
 
 elif operationType == "4":
-    if secondNumber == "0":
-        print("-------------------\nYou can't divide by 0, don't be a fool :)\n-------------------")
-    else: 
-        print("-------------------\nDivision it is! Result is " + str(int(firstNumber) / int(secondNumber)) + "\n-------------------")
-  
+# Activate Division flow
+    result = division(firstNumber, secondNumber)
+    print("-------------------\nDivision it is! Result is '" + result + "'\n-------------------")
+
 else: 
     print("-------------------\nInvalid option\n-------------------")
