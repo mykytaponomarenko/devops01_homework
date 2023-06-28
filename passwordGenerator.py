9238464a310ade6a425cd0c34312c196a43af75f
MykytaPonomarenko <mykyta.ponomarenko@gmail.com>
2023-06-28 18:45:01 +0300

    characters collected, printed

---
+++ passwordGenerator.py
@@ -0,0 +1,25 @@
+import string
+
+uppercase = string.ascii_uppercase
+lowercase = string.ascii_lowercase
+numbers = string.digits
+special = string.punctuation
+
+#print(type(uppercase))
+
+# shows all english uppercase letters
+#for u in uppercase:
+#    print(u)
+
+# shows all english lowercase letters
+#for u in lowercase:
+#    print(u)
+
+# shows 0-9
+# for u in numbers:
+#    print(u)
+
+# shows specials
+#print("There are " + str(len(special)) + " specials")
+#for u in special:
+#    print(u)
